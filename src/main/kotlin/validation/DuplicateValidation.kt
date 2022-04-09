1da43c1434003b48d4763a21c791db5902b28a67
Shikha Reddy <shikhar@sahaj.ai>
2022-04-09 16:03:08 +0530

    Added functionaity to check duplicates in json data

---
+++ src/main/kotlin/validation/DuplicateValidation.kt
@@ -0,0 +1,36 @@
+package validation
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+class DuplicateValidation {
+
+    fun checkDuplicates(jsonArray: JSONArray): JSONArray {
+        val mapOfJsonElements: MutableMap<String, Int> = mutableMapOf()
+        val jsonArrayOfDuplicateElements = JSONArray()
+        jsonArray.forEachIndexed { index, element ->
+            addElementToMap(mapOfJsonElements, element, index, jsonArrayOfDuplicateElements)
+        }
+        return jsonArrayOfDuplicateElements
+    }
+
+    private fun addElementToMap(
+        mapOfJsonElements: MutableMap<String, Int>,
+        element: Any,
+        index: Int,
+        jsonArrayOfDuplicateElements: JSONArray
+    ) {
+        if (mapOfJsonElements[element.toString()] == null) {
+            mapOfJsonElements[element.toString()] = index + 1
+            return
+        }
+
+        val jsonObject = JSONObject().put(
+            (index + 1).toString(),
+            "Row Duplicated From ${mapOfJsonElements[element.toString()]}"
+        )
+        jsonArrayOfDuplicateElements.put(jsonObject)
+
+    }
+
+}
---
+++ src/test/kotlin/validation/DuplicateTest.kt
@@ -0,0 +1,56 @@
+package validation
+
+import org.json.JSONArray
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class DuplicateTest {
+
+    @Test
+    fun shouldReturnEmptyMapForNoDuplicates() {
+        val duplicateValidation = DuplicateValidation()
+        val jsonString = "[{a : 1, b : 2, c : 3},{a : 1, b : 2, c : 4}]"
+        val jsonArray = JSONArray(jsonString)
+
+        val actual = duplicateValidation.checkDuplicates(jsonArray)
+
+        assertTrue(actual.isEmpty)
+    }
+
+    @Test
+    fun shouldReturnDuplicatePairsIfPresent() {
+        val duplicateValidation = DuplicateValidation()
+        val jsonString = "[{a : 1, b : 2, c : 3},{a : 1, b : 2, c : 3}]"
+        val jsonArray = JSONArray(jsonString)
+        val expected = JSONArray("[{2 : Row Duplicated From 1}]")
+
+        val actual = duplicateValidation.checkDuplicates(jsonArray)
+
+        assertEquals(expected.toList(), actual.toList())
+    }
+
+    @Test
+    fun shouldReturnAllDuplicatePairsIfPresent() {
+        val duplicateValidation = DuplicateValidation()
+        val jsonString = "[{a : 1, b : 2, c : 3},{a : 1, b : 2, c : 3},{a : 1, b : 2, c : 3} ]"
+        val jsonArray = JSONArray(jsonString)
+        val expected = JSONArray("[{2 : Row Duplicated From 1}, {3 : Row Duplicated From 1}]")
+
+        val actual = duplicateValidation.checkDuplicates(jsonArray)
+
+        assertEquals(expected.toList(), actual.toList())
+    }
+
+    @Test
+    fun shouldReturnDifferentDuplicatePairsIfPresent() {
+        val duplicateValidation = DuplicateValidation()
+        val jsonString = "[{a : 1, b : 2, c : 3},{a : 1, b : 2, c : 3},{a : 2, b : 2, c : 2}, {a : 2, b : 2, c : 2}]"
+        val jsonArray = JSONArray(jsonString)
+        val expected = JSONArray("[{2 : Row Duplicated From 1}, {4 : Row Duplicated From 3}]")
+
+        val actual = duplicateValidation.checkDuplicates(jsonArray)
+
+        assertEquals(expected.toList(), actual.toList())
+    }
+}
