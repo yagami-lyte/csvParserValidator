5566023455cbe3eac92ee254a7a4e2f2935b487a
shreyag@1 <shreyag@sahaj.ai>
2022-04-09 19:39:26 +0530

    added getPath() Functionality

--- src/main/kotlin/routeHandler/GetRouteHandler.kt
+++ src/main/kotlin/routeHandler/GetRouteHandler.kt
@@ -5,23 +5,33 @@ import java.io.File
 class GetRouteHandler {
 
     fun handleGetRequest(request: String): String {
-        val path = request.split("\r\n")[0].split(" ")[1]
+        val path = getPath(request)
         if (path == "/") {
             return getResponse("/index.html")
         }
         return getResponse("/404.html")
     }
 
+    private fun getPath(request: String): String {
+        val path = request.split("\r\n")[0].split(" ")[1]
+        return path
+    }
+
     private fun getResponse(path: String): String {
+        val body = getBodyResponse(path)
+        val httpHead = "HTTP/1.1 200 Found"
+        val contentLength = body.length
+        return httpHead + """Content-Type: text/html; charset=utf-8
+            |Content-Length: $contentLength""".trimMargin() + "\r\n\r\n" + body
+    }
+
+    private fun getBodyResponse(path: String): String {
         val filePath = System.getProperty("user.dir")
         var file = File("$filePath/src/main/public/$path")
         if (file.exists() === false) {
             var file = File("$filePath/src/main/public/404.html")
         }
-        val bodyResponse = file.readText(Charsets.UTF_8)
-        val httpHead = "HTTP/1.1 200 Found"
-        val contentLength = bodyResponse.length
-        return httpHead + """Content-Type: text/html; charset=utf-8
-            |Content-Length: $contentLength""".trimMargin() + "\r\n\r\n" + bodyResponse
+        return file.readText(Charsets.UTF_8)
+
     }
 }
