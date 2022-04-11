f4714d5621a76ddc1e3fc0c1d36bf47c7372ef4b
Gaurav Mishra <gauravm@sahaj.ai>
2022-04-11 13:16:19 +0530

    UI test- checked if elements are present

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
 describe("Connection testing", ()=>{
-    it("Connection with localhost", ()=>{
+    it("hitting the URL", ()=>{
         cy.visit('http://localhost:3000')
     })
+
+    it("Checking it certain elements present or not", ()=>{
+        cy.contains("Field").should("exist")
+        cy.contains("CSV PARSER")
+    })
 })
+
+describe("Typing data and checking field", ()=>{
+    it("Writing in config file",()=>{
+        cy.get("#field").type("productId")
+        cy.get("#type").select(1)
+    })
+})
+
