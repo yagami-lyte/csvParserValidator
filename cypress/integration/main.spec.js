2251dbd49947037b72e40283823184318b8553d5
Gaurav Mishra <gauravm@sahaj.ai>
2022-04-11 14:58:45 +0530

    Refactored the test code of UI-cypress
    Made it more independenet from id and class using customId, data-cy.

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Connection testing", ()=>{
-    it("hitting the URL", ()=>{
+    it("It loads successfully", ()=>{
         cy.visit('http://localhost:3000')
     })
 
@@ -11,14 +11,19 @@ describe("Connection testing", ()=>{
     })
 })
 
-describe("CSV Parser div", ()=>{
+describe("CSV Parser", ()=>{
     it("choosing file csv file",()=>{
-        cy.get("#csv_id").selectFile("src/main/public/assets/data.csv")
+        cy.get('[data-cy=chooseFile]').selectFile("src/main/public/assets/data.csv")
     })
 
     it("Uploading the file",()=>{
-        cy.get("#csv-submit-button").click()
+        cy.get('[data-cy=upload]').click()
     })
+
+    it("Choosing Non-CSV file",()=>{
+        cy.get('[data-cy=chooseFile]').not
+    })
+    
 })
 
 
