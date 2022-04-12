3a58958192842451ff0859ad8cd4bb05c32c4309
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-04-12 18:31:33 +0530

    Added Test for validateLength() Functionality()

--- src/main/kotlin/validation/LengthValidation.kt
+++ src/main/kotlin/validation/LengthValidation.kt
@@ -1,8 +1,40 @@
 package validation
 
+import JsonMetaDataTemplate
+import org.json.JSONArray
+import org.json.JSONObject
+
 class LengthValidation {
 
-    fun lengthCheck(data: String, length: Int):Boolean{
+    fun validateLength(jsonArrayData: JSONArray, fieldArray:Array<JsonMetaDataTemplate>): JSONArray {
+        val lengthErrors = JSONArray()
+        val lengthValidation = LengthValidation()
+
+        jsonArrayData.forEachIndexed { index, element ->
+            val fieldElement = (element as JSONObject)
+            val keys = fieldElement.keySet()
+            for (key in keys) {
+                val field = fieldArray.first { it.fieldName == key }
+                val value = fieldElement.get(key) as String
+                var flag = true
+                if (field.length != null && value.isNotEmpty()) {
+                    if (!lengthValidation.lengthCheck(value, field.length)) {
+                        flag = false
+                    }
+                }
+                if (!flag) {
+                    val jsonObject = JSONObject().put(
+                        (index + 1).toString(),
+                        "Incorrect length of ${field.fieldName}. Please change its length to ${field.length}"
+                    )
+                    lengthErrors.put(jsonObject)
+                }
+            }
+        }
+        return lengthErrors
+    }
+
+    private fun lengthCheck(data: String, length: Int):Boolean{
         return length == data.length
     }
 
--- src/test/kotlin/validation/LengthValidationTest.kt
+++ src/test/kotlin/validation/LengthValidationTest.kt
@@ -1,16 +1,27 @@
 package validation
 
+import org.json.JSONArray
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import routeHandler.PostRouteHandler
 
 internal class LengthValidationTest {
 
+    private val lengthValidation = LengthValidation()
+    private val metaData = """[{"fieldName":"Export","type":"Alphabets","length":"1","dependentOn":"","dependentValue":"","values":["Y","N"]},{"fieldName":"Country Name","type":"Alphabets","length":"3","dependentOn":"Export","dependentValue":"N","values":["Export,Country Name","Y,","N,USA",""]}]"""
+
     @Test
-    fun shouldBeAbleToValidateFixedLength() {
-        val lengthValidation = LengthValidation()
+    fun shouldPerformDependencyCheck() {
 
-        val actual = lengthValidation.lengthCheck("gftgd", 5)
+        val postRouteHandler = PostRouteHandler()
+        val jsonData = postRouteHandler.getMetaData(metaData)
+        postRouteHandler.fieldArray = jsonData
+        val csvData = """[{"Export":"Y","Country Name":"INDIA"},{"Export":"N","Country Name":"USA"}]"""
+        val jsonCsvData = JSONArray(csvData)
+        val expected = JSONArray("[{\"1\":\"Incorrect length of Country Name. Please change its length to 3\"}]")
 
-        assertTrue(actual)
+        val actual = lengthValidation.validateLength(jsonCsvData , postRouteHandler.fieldArray)
+
+        assertEquals(expected.toString(), actual.toString())
     }
 }
