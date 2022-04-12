0e4e2e0546b4c9b7f6afa73baa18d0ec1b50f092
Gaurav Mishra <gauravm@sahaj.ai>
2022-04-13 00:15:04 +0530

    Refactored the failing test

--- src/test/kotlin/validation/LengthValidationTest.kt
+++ src/test/kotlin/validation/LengthValidationTest.kt
@@ -8,15 +8,15 @@ import routeHandler.PostRouteHandler
 internal class LengthValidationTest {
 
     private val lengthValidation = LengthValidation()
-    private val metaData =
-        """[{"fieldName":"Export","type":"Alphabets","length":"1","dependentOn":"","dependentValue":"","values":["Y","N"]},{"fieldName":"Country Name","type":"Alphabets","length":"3","dependentOn":"Export","dependentValue":"N","values":["IND","USA","AUS"]}]"""
-    private val csvData = """[{"Export":"Y","Country Name":"INDIA"},{"Export":"N","Country Name":"USA"}]"""
 
     @Test
     fun shouldPerformLengthValidationCheck() {
+        val metaData =
+            """[{"fieldName":"Export","type":"Alphabets","length":"1","dependentOn":"","dependentValue":"","values":["Y","N"]},{"fieldName":"Country Name","type":"Alphabets","length":"3","dependentOn":"Export","dependentValue":"N","values":["IND","USA","AUS"]}]"""
         val postRouteHandler = PostRouteHandler()
         val jsonData = postRouteHandler.getMetaData(metaData)
         postRouteHandler.fieldArray = jsonData
+        val csvData = """[{"Export":"Y","Country Name":"INDIA"},{"Export":"N","Country Name":"USA"}]"""
         val jsonCsvData = JSONArray(csvData)
         val expected = JSONArray("[{\"1\":\"Incorrect length of Country Name. Please change its length to 3\"}]")
 
@@ -27,9 +27,13 @@ internal class LengthValidationTest {
 
     @Test
     fun shouldReturnJsonArrayWithMultipleErrors() {
+        val metaData =
+            """[{"fieldName":"Export","type":"Alphabets","length":"2","dependentOn":"","dependentValue":"","values":["Y","N"]},{"fieldName":"Country Name","type":"Alphabets","length":"3","dependentOn":"Export","dependentValue":"N","values":["IND","USA","AUS"]}]"""
         val postRouteHandler = PostRouteHandler()
         val jsonData = postRouteHandler.getMetaData(metaData)
         postRouteHandler.fieldArray = jsonData
+        val csvData =
+            """[{"Export":"Y","Country Name":""},{"Export":"Y","Country Name":""},{"Export":"N","Country Name":"USA"}]"""
         val jsonCsvData = JSONArray(csvData)
         val expected =
             JSONArray("[{\"1\":\"Incorrect length of Export. Please change its length to 2\"},{\"2\":\"Incorrect length of Export. Please change its length to 2\"},{\"3\":\"Incorrect length of Export. Please change its length to 2\"}]")
@@ -41,9 +45,13 @@ internal class LengthValidationTest {
 
     @Test
     fun shouldReturnJsonArrayWithNoErrors() {
+        val metaData =
+            """[{"fieldName":"Export","type":"Alphabets","length":"1","dependentOn":"","dependentValue":"","values":["Y","N"]},{"fieldName":"Country Name","type":"Alphabets","length":"3","dependentOn":"Export","dependentValue":"N","values":["IND","USA","AUS"]}]"""
         val postRouteHandler = PostRouteHandler()
         val jsonData = postRouteHandler.getMetaData(metaData)
         postRouteHandler.fieldArray = jsonData
+        val csvData =
+            """[{"Export":"Y","Country Name":"AUS"},{"Export":"Y","Country Name":"IND"},{"Export":"N","Country Name":"USA"}]"""
         val jsonCsvData = JSONArray(csvData)
         val expected = JSONArray("[]")
 
