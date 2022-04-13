be8c956a3bc5fc4d91edfd0eac524c7776570f6b
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-04-13 16:36:37 +0530

    Added integration test for Server

---
+++ src/test/kotlin/ServerIntegrationTest.kt
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import routeHandler.PostRouteHandler
+import java.net.Socket
+
+class ServerIntegrationTest {
+
+    private lateinit var server: Server
+    @Test
+    fun shouldReturn200ResponseWhenLoadingHomePage() {
+        val port = 3012
+        startServerInThread(port)
+        val clientSocket = Socket("localhost", port)
+        val outputStream = clientSocket.getOutputStream()
+        val inputStream = clientSocket.getInputStream()
+        val request = """GET / HTTP/1.1 
+                |Host: localhost:3000""".trimMargin() + "\r\n\r\n"
+        outputStream.write(request.toByteArray())
+        val expectedResponseCode = "200"
+        val response = String(inputStream.readAllBytes())
+
+        val actualResponseCode = extractResponseCode(response)
+        clientSocket.close()
+
+        assertEquals(expectedResponseCode, actualResponseCode)
+    }
+
+    private fun extractResponseCode(response: String): String {
+        return response.split(" ")[1]
+    }
+
+    private fun startServerInThread(port: Int) {
+        Thread {
+            server = Server(port)
+            server.startServer()
+        }.start()
+    }
+}
