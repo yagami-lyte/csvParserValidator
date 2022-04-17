20df435360bb0010600132ecb989618588279741
shreyag@1 <shreyag@sahaj.ai>
2022-04-17 19:17:54 +0530

    Refactored ValueValidation class

--- src/main/kotlin/validation/ValueValidation.kt
+++ src/main/kotlin/validation/ValueValidation.kt
@@ -9,19 +9,42 @@ class ValueValidation : Validation {
     override fun validate(dataInJSONArray: JSONArray, fieldArray: Array<ConfigurationTemplate>): JSONArray {
         val valueErrors = JSONArray()
         dataInJSONArray.forEachIndexed { index, element ->
-            val ele = (element as JSONObject)
-            val keys = ele.keySet()
-            for (key in keys) {
-                val field = fieldArray.first { it.fieldName == key }
-                val value = ele.get(key) as String
-                val flag = checkIfValueIsIncorrect(field, value)
-                if (!flag) {
-                    val jsonObject = errorMessage(index, field)
-                    valueErrors.put(jsonObject)
-                }
-            }
+            valueValidationForEachRow(element as JSONObject, fieldArray, index, valueErrors)
         }
         return valueErrors
+    }
+
+    private fun valueValidationForEachRow(
+        element: Any?,
+        fieldArray: Array<ConfigurationTemplate>,
+        index: Int,
+        valueErrors: JSONArray
+    ) {
+        val (ele, keys) = getElementKeys(element)
+        for (key in keys) {
+            val (field, value) = getFieldValues(fieldArray, key, ele)
+            val flag = checkIfValueIsIncorrect(field, value)
+            getErrorMessages(flag, index, field, valueErrors)
+        }
+    }
+
+    private fun getElementKeys(element: Any?): Pair<JSONObject, MutableSet<String>> {
+        val ele = (element as JSONObject)
+        val keys = ele.keySet()
+        return Pair(ele, keys)
+    }
+
+    private fun getFieldValues(fieldArray: Array<ConfigurationTemplate>, key: String?, ele: JSONObject): Pair<ConfigurationTemplate, String> {
+        val field = fieldArray.first { it.fieldName == key }
+        val value = ele.get(key) as String
+        return Pair(field, value)
+    }
+
+    private fun getErrorMessages(flag: Boolean, index: Int, field: ConfigurationTemplate, valueErrors: JSONArray) {
+        if (!flag) {
+            val jsonObject = errorMessage(index, field)
+            valueErrors.put(jsonObject)
+        }
     }
 
     fun valueCheck(allowedValues: List<String>?, value: String): Boolean {
