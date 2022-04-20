208c1ac1d2b6d3a0ab54b92ccd5f471dea5917ff
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-04-21 00:11:37 +0530

    Added Floating Number in the Dropdown

--- src/main/public/main.js
+++ src/main/public/main.js
@@ -53,6 +53,7 @@ function showColFields(lines){
                                 <option value="Number">Number</option>
                                 <option value="AlphaNumeric">AlphaNumeric</option>
                                 <option value="Alphabets">Alphabets</option>
+                                <option value="Floating Number">Floating Number</option>
                                 <option value="Date Time">Date Time</option>
                                 <option value="Email">Email</option>
                             </select>
