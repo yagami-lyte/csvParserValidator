9a44f609b6f750a47fa044040466fe52a6953524
shreyag@1 <shreyag@sahaj.ai>
2022-04-29 01:14:53 +0530

    Added prePendingZeros Functionality

--- src/main/kotlin/validation/PrependingZeroesValidation.kt
+++ src/main/kotlin/validation/PrependingZeroesValidation.kt
@@ -1,7 +1,35 @@
 package validation
 
-class PrependingZeroesValidation {
+import jsonTemplate.ConfigurationTemplate
+import org.json.JSONArray
+import org.json.JSONObject
 
+class PrependingZeroesValidation:Validation {
+
+    override fun validate(jsonArrayData: JSONArray, fieldArray: Array<ConfigurationTemplate>): JSONArray {
+        val prePendingErrors = JSONArray()
+
+        jsonArrayData.forEachIndexed { index, element ->
+            val ele = (element as JSONObject)
+            val keys = ele.keySet()
+            for (key in keys) {
+                val field = fieldArray.first { it.fieldName == key }
+                var flag = true
+                val value = ele.get(key) as String
+                if (field.type == "Number" && value.isNotEmpty() && checkPrePendingZeros(value)) {
+                    flag = false
+                }
+                if (!flag) {
+                    val jsonObject = JSONObject().put(
+                        (index + 1).toString(),
+                        "Incorrect Type of ${field.fieldName}. Please remove PrePending Zeros!"
+                    )
+                    prePendingErrors.put(jsonObject)
+                }
+            }
+        }
+        return prePendingErrors
+    }
 
     fun checkPrePendingZeros(value: String): Boolean {
         return value.startsWith("0")
