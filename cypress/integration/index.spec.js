425f6da3f52e18de51f6f9b8ebb94f7a0cdff2cf
shreyag@1 <shreyag@sahaj.ai>
2022-05-01 13:58:20 +0530

    Added cypress test for load server successfully

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import 'cypress-file-upload';
 
 describe("Testing Upload CSV", ()=>{
 
@@ -6,17 +7,7 @@ describe("Testing Upload CSV", ()=>{
         cy.visit('http://localhost:3002')
     })
 
-    it("Should contain Upload your CSV file here ", ()=>{
-        cy.contains("Upload your CSV file here")
-    })
 
-//     it('Should take csv as input',()=> {
-//        cy.get('#csv_id').selectFile('cypress/fixtures/countries.csv',{force: true})
-//        cy.get('input[type="file"]').attachFile('countries.csv');
-//     })
-//
-//     it('Should contain button to navigate to config section', () => {
-//        cy.get('#uploadCSV').click({force: true})
-//        cy.visit('http://localhost:3002/#config')
-//    })
+
+
 })
