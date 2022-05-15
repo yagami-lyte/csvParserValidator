ce77f28b1ebd9d4e0f65f02fa2a6f60d64a70674
Shikha Reddy <shikhar@sahaj.ai>
2022-05-16 02:21:43 +0530

    Bug fix: Cypress tests

--- cypress/integration/config.spec.js
+++ cypress/integration/config.spec.js
@@ -3,11 +3,11 @@
 describe("Test for configuration body",()=>{
 
     it("Should load server successfully", ()=>{
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3004')
     })
 
     it("Should contain the Configuration Header",()=>{
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3004')
         cy.contains("Configure your CSV fields").should('exist')
         
     })
--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,19 +1,19 @@
-/// <reference types="cypress" />
+// <reference types="cypress" />
 import 'cypress-file-upload';
 
 describe("Testing Upload CSV", ()=>{
 
     it("Should load server successfully", ()=>{
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3004')
     })
 
     it("Should contain the header",()=>{
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3004')
         cy.contains("CSV Parser And Validator").should('exist')
     })
 
     it("Should contain the body", ()=>{
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3004')
         cy.contains("Upload your CSV file here").should('exist')
         cy.contains("Upload").should('exist')
         cy.get('div#upload').should('exist')
@@ -21,7 +21,7 @@ describe("Testing Upload CSV", ()=>{
     })
 
      it("Links in the body should work properly",()=>{
-         cy.visit('http://localhost:3002')
+         cy.visit('http://localhost:3004')
          cy.get('input[type="file"]').attachFile("countries.csv")
      })
 
