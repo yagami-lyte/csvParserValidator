e566233fcd97fcc7d84dd93d1c266a6cf2c18d2c
Suraj Bokde <surajb@sahaj.ai>
2022-05-18 00:40:07 +0530

    modified tables of database

--- src/main/kotlin/database/databaseTables.sql
+++ src/main/kotlin/database/databaseTables.sql
@@ -1,34 +1,38 @@
-CREATE TABLE csv_files
+DROP TABLE IF EXISTS field_values;
+DROP TABLE IF EXISTS csv_fields;
+DROP TABLE IF EXISTS csv_files;
+
+CREATE TABLE configuration
 (
-    csv_name VARCHAR(255)  UNIQUE
+    config_id   INT NOT NULL AUTO_INCREMENT,
+    config_name VARCHAR(255) UNIQUE,
+    PRIMARY KEY (config_id)
 );
 
 CREATE TABLE csv_fields
 (
-     entry_date    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     csv_name      VARCHAR(255),
+     field_id   INT AUTO_INCREMENT,
+     config_id  INT,
      field_name    VARCHAR(255) NOT NULL,
      field_type    VARCHAR(255) ,
      is_null_allowed VARCHAR(255),
      field_length  VARCHAR(255),
-     field_values  VARCHAR(255),
      dependent_field VARCHAR(255),
      dependent_value VARCHAR(255),
      date_type VARCHAR(255),
      time_type VARCHAR(255),
-     datetime VARCHAR(255),
-     CONSTRAINT fk_config
-        FOREIGN KEY (csv_name)
-            REFERENCES csv_files(csv_name)
+     datetime_type VARCHAR(255),
+     PRIMARY KEY (field_id),
+         FOREIGN KEY (config_id) REFERENCES configuration (config_id)
 );
 
 CREATE TABLE field_values
 (
-    csv_name  VARCHAR(255),
-    field_name VARCHAR(255),
-    allowed_value VARCHAR(255),
-    CONSTRAINT fkey_csv
-        FOREIGN KEY (csv_name)
-            REFERENCES csv_fields(csv_name)
-            ON DELETE CASCADE
+    value_id INT AUTO_INCREMENT,
+    allowed_value VARCHAR(255) NOT NULL,
+    field_id INT,
+    PRIMARY KEY (value_id),
+    FOREIGN KEY (field_id) REFERENCES csv_fields (field_id)
 );
+
+
