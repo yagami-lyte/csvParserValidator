88f51f0265101b529412e916fb8941242da6de0c
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-05-19 01:04:51 +0530

    Added test for readConfiguration() Functionlaity()

--- src/test/kotlin/database/DatabaseOperationsTest.kt
+++ src/test/kotlin/database/DatabaseOperationsTest.kt
@@ -48,23 +48,37 @@ internal class DatabaseOperationsTest {
     @Test
     fun shouldNotBeAbleToAddTheFieldsInTheDatabase() {
         val databaseOperations = DatabaseOperations(TestConnector())
-        val configName = "configuration"
+        val configurationName = "testConfiguration"
         val jsonData = createJsonTemplate()
-        databaseOperations.saveNewConfigurationInDatabase(configName)
+        databaseOperations.saveNewConfigurationInDatabase(configurationName)
         jsonData.forEach {
-            databaseOperations.writeConfiguration(configName,it)
+            databaseOperations.writeConfiguration(configurationName,it)
         }
         val fieldName = "abc"
         val result = databaseOperations.isFieldPresentInTheDatabase(fieldName)
         assertFalse(result)
+    }
 
+    @Test
+    fun shouldBeAbleToReadConfigDataFromDatabase() {
+        val databaseOperations = DatabaseOperations(TestConnector())
+        val configurationName = "testConfiguration"
+        val jsonData = createJsonTemplate()
+        databaseOperations.saveNewConfigurationInDatabase(configurationName)
+        jsonData.forEach {
+            databaseOperations.writeConfiguration(configurationName,it)
+        }
+        val actualConfigurationData = databaseOperations.readConfiguration(configurationName)
+        val expectedFieldName = jsonData.first().fieldName
+
+        val actualFieldName = actualConfigurationData.first().fieldName
+
+        assertEquals(expectedFieldName , actualFieldName)
     }
 
     private fun createJsonTemplate(): Array<ConfigurationTemplate> {
         val metaData =
-            """[{"fieldName":"Export Number","type":"Number","length":"","dependentOn":"","dependentValue":"","values":[]},{"fieldName":"Country Name","type":"Alphabets","length":"4","dependentOn":"Export","dependentValue":"N","values":["Export,Country Name","Y,","N,USA",""]}]"""
+            """[{"fieldName":"Export Number","type":"Number","length":"","dependentOn":"","dependentValue":"",nullValue: "Allowed"},{"fieldName":"Country Name","type":"Alphabets","length":"4","dependentOn":"Export",nullValue: "Allowed","dependentValue":"N"}]"""
         return Gson().fromJson(metaData, Array<ConfigurationTemplate>::class.java)
     }
-
-
 }
