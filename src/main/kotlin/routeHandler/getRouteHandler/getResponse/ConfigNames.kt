ba7928fc6eacc1d7fed9d0507052496a4a35af06
shreyag@1 <shreyag@sahaj.ai>
2022-05-21 00:44:46 +0530

    Added ConfigNames functionality and tests

---
+++ src/main/kotlin/routeHandler/getRouteHandler/getResponse/ConfigNames.kt
@@ -0,0 +1,41 @@
+package routeHandler.getRouteHandler.getResponse
+
+import database.Connector
+import database.DatabaseOperations
+import org.json.JSONObject
+import routeHandler.ResponseHeader
+import routeHandler.StatusCodes
+
+class ConfigNames(private val databaseOperations: DatabaseOperations):GetResponse {
+
+    private val responseHeader = ResponseHeader()
+
+    override fun getResponse(path:String): String{
+        val responseBody = getConfigResponse()
+        val contentLength = responseBody.length
+        val endOfHeader = "\r\n\r\n"
+        return responseHeader.getResponseHead(StatusCodes.TWOHUNDRED) + """Content-Type: text/json; charset=utf-8
+                |Content-Length: $contentLength""".trimMargin() + endOfHeader + responseBody
+    }
+
+    private fun getConfigResponse(): String {
+        val configFiles = databaseOperations.getConfigNames()
+        val configJsonArrayResponse = prepareJsonResponse(configFiles)
+        var responseBody = "{"
+        responseBody += "\"configFiles\" : $configJsonArrayResponse"
+        responseBody += "}"
+        println("responsecofig : $responseBody")
+        return responseBody
+    }
+
+    private fun prepareJsonResponse(configDataTemplate: List<String>?): JSONObject {
+        var countOfConfig = 1
+        val jsonObject = JSONObject()
+        configDataTemplate?.forEach {
+            jsonObject.put("$countOfConfig", it)
+            countOfConfig += 1
+        }
+
+        return jsonObject
+    }
+}
---
+++ src/test/kotlin/routeHandler/getRouteHandler/getResponse/ConfigNamesTest.kt
@@ -0,0 +1,56 @@
+package routeHandler.getRouteHandler.getResponse
+
+import database.DatabaseOperations
+import database.TestConnector
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class ConfigNamesTest {
+    private val testConfigurationName1 = "testConfigurationName1"
+    private val testConfigurationName2 = "testConfigurationName2"
+
+    @Test
+    fun shouldBeAbleToGetConfigurationNamesForConfigNameGetRequest() {
+        val configNamesPath = "/get-config-files"
+        val databaseOperations = DatabaseOperations(TestConnector())
+        val testConfigurationName1 = "testConfigurationName1"
+        databaseOperations.saveNewConfigurationInDatabase(testConfigurationName1)
+        val configNames = ConfigNames(databaseOperations)
+        val expectedConfigNames = """{"configFiles" : {"1":"testConfigurationName1"}}"""
+        val response = configNames.getResponse(configNamesPath)
+
+        val actualConfigNames = response.split("\r\n")[2]
+
+        assertEquals(expectedConfigNames, actualConfigNames)
+    }
+
+    @Test
+    fun shouldBeAbleToGetMultipleConfigurationNamesForConfigNameGetRequest() {
+        val configNamesPath = "/get-config-files"
+        val databaseOperations = DatabaseOperations(TestConnector())
+        databaseOperations.saveNewConfigurationInDatabase(testConfigurationName1)
+        databaseOperations.saveNewConfigurationInDatabase(testConfigurationName2)
+        val configNames = ConfigNames(databaseOperations)
+        val expectedConfigNames = """{"configFiles" : {"1":"testConfigurationName1","2":"testConfigurationName2"}}"""
+        val response = configNames.getResponse(configNamesPath)
+
+        val actualConfigNames = response.split("\r\n")[2]
+
+        assertEquals(expectedConfigNames, actualConfigNames)
+    }
+
+    @Test
+    fun shouldBeAbleToGetZeroConfigurationNamesForConfigNameGetRequest() {
+        val configNamesPath = "/get-config-files"
+        val databaseOperations = DatabaseOperations(TestConnector())
+        val configNames = ConfigNames(databaseOperations)
+        val expectedConfigNames = """{"configFiles" : {}}"""
+        val response = configNames.getResponse(configNamesPath)
+
+        val actualConfigNames = response.split("\r\n")[2]
+
+        assertEquals(expectedConfigNames, actualConfigNames)
+    }
+
+
+}
