ff099c9eb37890a7bf397e20bfeb34e3587883fe
shreyag@1 <shreyag@sahaj.ai>
2022-05-21 00:14:25 +0530

    Modified Servertest

--- src/test/kotlin/server/ServerTest.kt
+++ src/test/kotlin/server/ServerTest.kt
@@ -1,59 +1,40 @@
 package server
 
-import io.mockk.every
-import io.mockk.mockk
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import java.io.BufferedReader
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.InputStreamReader
 import java.net.Socket
 
 class ServerTest {
 
+    @Test
+    fun shouldReturnTrueIfTheServerHasStarted() {
+        val port = 3000
+        var actual = true
+        Server(port)
+
+        try {
+            val socket = Socket("localhost",port)
+            socket.close()
+        } catch (e: Exception) {
+            actual = false
+        }
+
+        assertTrue(actual)
+    }
 
     @Test
-    fun shouldBeAbleToGetResponseForGetRequest() {
-        val server = Server(3010)
-        val request = """GET / HTTP/1.1 
-                |Host: localhost:3010""".trimMargin() + "\r\n\r\n"
-        val csvData = """[{"Export":"Y","Country Name":""},{"Export":"N","Country Name":"USA"}]"""
-        val mockSocket = createMockSocket(csvData)
-        val inputStream = BufferedReader(InputStreamReader(mockSocket.getInputStream()))
-        val expectedResponseCode = "200"
-        val response = server.handleRequest(request, inputStream)
+    fun shouldReturnFalseIfTheServerHasNotStarted() {
+        val port = 3000
+        var actual = true
 
-        val actualResponseCode = extractResponseCode(response)
+        try {
+            val socket = Socket("localhost",port)
+            socket.close()
+        } catch (e: Exception) {
+            actual = false
+        }
 
-        assertEquals(expectedResponseCode, actualResponseCode)
-    }
-
-    @Test
-    fun shouldBeAbleToReturnPageNotFoundResponse() {
-        val server = Server(308)
-        val request = """GET /113 HTTP/1.1 
-                |Host: localhost:3008""".trimMargin() + "\r\n\r\n"
-        val csvData = """[{"Export":"Y","Country Name":""},{"Export":"N","Country Name":"USA"}]"""
-        val mockSocket = createMockSocket(csvData)
-        val inputStream = BufferedReader(InputStreamReader(mockSocket.getInputStream()))
-        val expectedResponseCode = "404"
-        val response = server.handleRequest(request, inputStream)
-
-        val actualResponseCode = extractResponseCode(response)
-
-        assertEquals(expectedResponseCode, actualResponseCode)
-    }
-
-
-    private fun createMockSocket(csvData: String): Socket {
-        val mockSocket = mockk<Socket>()
-        every { mockSocket.getOutputStream() } returns ByteArrayOutputStream()
-        every { mockSocket.getInputStream() } returns ByteArrayInputStream(csvData.toByteArray())
-        return mockSocket
-    }
-
-    private fun extractResponseCode(response: String): String {
-        return response.split("\n")[0].split(" ")[1]
+        assertFalse(actual)
     }
 }
