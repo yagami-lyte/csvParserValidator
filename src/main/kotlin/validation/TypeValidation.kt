5a8cf55be7548836333ba5d397f2a6a0e85dbf24
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-05-22 23:18:53 +0530

    TypeValidation Class Refactored

--- src/main/kotlin/validation/TypeValidation.kt
+++ src/main/kotlin/validation/TypeValidation.kt
@@ -1,16 +1,31 @@
 package validation
 
+import TypeEnum
 import jsonTemplate.ConfigurationTemplate
 import org.json.JSONArray
 import org.json.JSONObject
-import java.text.DateFormat
-import java.text.ParseException
-import java.text.SimpleDateFormat
 
 
 class TypeValidation : Validation {
 
     private val mapOfTypeErrors = mutableMapOf<String , MutableList<String>>()
+
+    private fun validateTypeInEachRow(
+        field: ConfigurationTemplate,
+        value: String,
+    ): Boolean {
+        var isTypeValid = true
+        val typeMap = mapOf(
+            "DateTime"  to field.datetime,
+            "Date" to field.date,
+            "Time" to field.time,
+        )
+
+        if(value.isNotEmpty() && !TypeEnum.valueOf(field.type.toString()).typeCheck(value,typeMap.getOrDefault(field.type.toString(),""))) {
+            isTypeValid = false
+        }
+        return isTypeValid
+    }
 
     override fun validate(jsonArrayData: JSONArray, fieldArray: Array<ConfigurationTemplate>): MutableMap<String, MutableList<String>> {
         mapOfTypeErrors.clear()
@@ -23,35 +38,6 @@ class TypeValidation : Validation {
             }
         }
         return mapOfTypeErrors
-    }
-
-    private fun validateTypeInEachRow(
-        field: ConfigurationTemplate,
-        value: String,
-    ): Boolean {
-        var isTypeValid = true
-        if (field.type == "AlphaNumeric" && value.isNotEmpty() && !isAlphaNumeric(value)) {
-            isTypeValid = false
-        } else if (field.type == "Alphabets" && value.isNotEmpty() && !isAlphabetic(value)) {
-            isTypeValid = false
-        } else if (field.type == "Number" && value.isNotEmpty() && !isNumeric(value)) {
-            isTypeValid = false
-        } else if (field.type == "Date Time" && value.isNotEmpty() && !checkDateOrTimeFormat(field.datetime, value)) {
-            isTypeValid = false
-        } else if (field.type == "Date" && value.isNotEmpty() && !checkDateOrTimeFormat(field.date, value)) {
-            isTypeValid = false
-        } else if (field.type == "Time" && value.isNotEmpty() && !checkDateOrTimeFormat(field.time, value)) {
-            isTypeValid = false
-        } else if (field.type == "Email" && value.isNotEmpty() && !isEmail(value)) {
-            isTypeValid = false
-        } else if (field.type == "Floating Number" && value.isNotEmpty() && !isFloatingNumber(value)) {
-            isTypeValid = false
-        } else if (field.type == "Text" && value.isNotEmpty() && !hasText(value)) {
-            isTypeValid = false
-        }
-
-
-        return isTypeValid
     }
 
     private fun getFieldValues(
@@ -83,44 +69,4 @@ class TypeValidation : Validation {
             mapOfTypeErrors[field.fieldName]?.add((index+2).toString())
         }
     }
-
-    fun isFloatingNumber(value: String): Boolean {
-        return value.matches("[+-]?([0-9]*[.])?[0-9]+".toRegex())
-    }
-
-    fun isNumeric(value: String): Boolean {
-        return value.matches("-?\\d+(\\.\\d+)?".toRegex())
-    }
-
-    fun isAlphabetic(value: String): Boolean {
-        return value.all { it.isLetter() }
-    }
-
-    fun isAlphaNumeric(value: String): Boolean {
-        return value.all { it.isLetterOrDigit() }
-    }
-
-    fun hasText(value: String): Boolean {
-        return true
-//        val allKeyboardKeysRegex = ("""[A-Za-z0-9-]+[ 0-9A-Za-z#$%=@!{},`~&*()'<>?.:;_|^/+\t\r\n\[\]"-]*""").toRegex();
-//        return allKeyboardKeysRegex.matches(value)
-    }
-
-    fun isEmail(value: String): Boolean {
-        val emailPattern = Regex("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\$")
-        return emailPattern.matches(value)
-    }
-
-    fun checkDateOrTimeFormat(dateTimeFormat: String?, value: String): Boolean {
-        val sdf: DateFormat = SimpleDateFormat(dateTimeFormat as String)
-        sdf.isLenient = false
-        try {
-            sdf.parse(value.trim())
-        } catch (e: ParseException) {
-            return false
-        }
-        return true
-    }
-
-
 }
