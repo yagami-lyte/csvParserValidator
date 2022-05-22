b67fb3f68c9413ef170d5bcf9f3e642bbcf0efd0
shreyag@1 <shreyag@sahaj.ai>
2022-05-23 00:36:25 +0530

    Added HandleCSVMetaData functionality and its tests

---
+++ src/main/kotlin/routeHandler/postRouteHandler/postResponse/HandleCSVMetaData.kt
@@ -0,0 +1,63 @@
+package routeHandler.postRouteHandler.postResponse
+
+import com.google.gson.Gson
+import database.Connector
+import database.DatabaseOperations
+import jsonTemplate.ConfigurationTemplate
+import routeHandler.ResponseHeader
+import routeHandler.StatusCodes
+import java.io.BufferedReader
+
+class HandleCSVMetaData(var fieldArray: Array<ConfigurationTemplate> = arrayOf()) : PostResponse {
+
+    private val responseHeader = ResponseHeader()
+
+    override fun postResponse(request: String, inputStream: BufferedReader): String {
+        val bodySize = getContentLength(request)
+        val body = getBody(bodySize, inputStream)
+        return getResponseForMetaData(body)
+    }
+
+    private fun getContentLength(request: String): Int {
+        request.split("\n").forEach { headerString ->
+            val keyValue = headerString.split(":", limit = 2)
+            if (keyValue[0].contains("Content-Length")) {
+                return keyValue[1].trim().toInt()
+            }
+        }
+        return 0
+    }
+
+    private fun getBody(bodySize: Int, inputStream: BufferedReader): String {
+        val buffer = CharArray(bodySize)
+        inputStream.read(buffer)
+        return String(buffer)
+    }
+
+    private fun getResponseForMetaData(body: String): String {
+        val jsonBody = getMetaData(body)
+        fieldArray = jsonBody
+        val configName = fieldArray.first().configName
+        val databaseOperations = DatabaseOperations(Connector())
+        println(configName)
+        if (configName != null && configName != "") {
+            databaseOperations.saveNewConfigurationInDatabase(configName)
+            fieldArray.forEach {
+                databaseOperations.writeConfiguration(configName, it)
+            }
+        }
+        val endOfHeader = "\r\n\r\n"
+        val responseBody = "Successfully Added Configuration File"
+        val contentLength = responseBody.length
+        return responseHeader.getResponseHead(StatusCodes.TWOHUNDRED) + """Content-Type: text/plain; charset=utf-8
+    |Content-Length: $contentLength""".trimMargin() + endOfHeader + responseBody
+    }
+
+    fun getMetaData(body: String): Array<ConfigurationTemplate> {
+        val gson = Gson()
+        return gson.fromJson(body, Array<ConfigurationTemplate>::class.java)
+    }
+
+
+
+}
