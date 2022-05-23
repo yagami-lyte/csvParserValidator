c9fec6d1fa6dfa90435f2fbb7254126cf48a041f
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-05-23 15:51:31 +0530

    SendConfigurations() class refactored

--- src/main/kotlin/routeHandler/postRouteHandler/postResponse/SendConfigurations.kt
+++ src/main/kotlin/routeHandler/postRouteHandler/postResponse/SendConfigurations.kt
@@ -20,8 +20,7 @@ class SendConfigurations(private val databaseOperations: DatabaseOperations) : P
         return getResponseForConfig(body)
     }
 
-
-    fun getResponseForConfig(body: String): String {
+    private fun getResponseForConfig(body: String): String {
         val responseBody = getConfigResponse(body)
         val contentLength = responseBody.length
         val endOfHeader = "\r\n\r\n"
@@ -30,35 +29,35 @@ class SendConfigurations(private val databaseOperations: DatabaseOperations) : P
     }
 
     private fun getConfigResponse(body: String): String {
-        println("body $body")
         val configName = body.split('"')[3]
-        println("configName $configName")
         val configDataTemplate = databaseOperations.readConfiguration(configName)
         val configJsonArrayResponse = prepareJsonResponse(configDataTemplate)
-        var responseBody = "{"
-        responseBody += "\"Type\" : $configJsonArrayResponse"
-        responseBody += "}"
-        return responseBody
+        return "{\"Type\" : $configJsonArrayResponse}"
     }
 
     private fun prepareJsonResponse(configDataTemplate: Array<ConfigurationTemplate>): JSONArray {
         val jsonArrayOfConfigData = JSONArray()
-        configDataTemplate.forEach {
-            val jsonObject = JSONObject()
-            jsonObject.put("type", it.type)
-            jsonObject.put("length", it.length)
-            jsonObject.put("dateTime", it.datetime)
-            jsonObject.put("date", it.date)
-            jsonObject.put("time", it.time)
-            jsonObject.put("values", it.values)
-            jsonObject.put("dependentOn", it.dependentOn)
-            jsonObject.put("dependentValue", it.dependentValue)
-            jsonObject.put("nullValue", it.nullValue)
 
-            val fieldJsonObject = JSONObject().put(it.fieldName, jsonObject)
-            jsonArrayOfConfigData.put(fieldJsonObject)
+        configDataTemplate.map {
+            val jsonObject = prepareJsonObjectForField(it)
+            val fieldConfig = JSONObject().put(it.fieldName, jsonObject)
+            jsonArrayOfConfigData.put(fieldConfig)
         }
-        println("jsonArrayOfConfigData $jsonArrayOfConfigData")
+
         return jsonArrayOfConfigData
+    }
+
+    private fun prepareJsonObjectForField(it: ConfigurationTemplate): JSONObject {
+        val jsonObject = JSONObject()
+        jsonObject.put("type", it.type)
+        jsonObject.put("length", it.length)
+        jsonObject.put("dateTime", it.datetime)
+        jsonObject.put("date", it.date)
+        jsonObject.put("time", it.time)
+        jsonObject.put("values", it.values)
+        jsonObject.put("dependentOn", it.dependentOn)
+        jsonObject.put("dependentValue", it.dependentValue)
+        jsonObject.put("nullValue", it.nullValue)
+        return jsonObject
     }
 }
