7f7deafae28b9a51b7353c67801686eff1471aba
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-05-24 14:59:24 +0530

    PrependingZeroesValidation Class Refactored

--- src/main/kotlin/validation/PrependingZeroesValidation.kt
+++ src/main/kotlin/validation/PrependingZeroesValidation.kt
@@ -10,30 +10,43 @@ class PrependingZeroesValidation : Validation {
 
     override fun validate(
         jsonArrayData: JSONArray,
-        fieldArray: Array<ConfigurationTemplate>
+        fieldArray: Array<ConfigurationTemplate>,
     ): MutableMap<String, MutableList<String>> {
 
         mapOfPrePendingErrors.clear()
         jsonArrayData.forEachIndexed { index, element ->
-            val ele = (element as JSONObject)
-            val keys = ele.keySet()
-            for (key in keys) {
-                val field = fieldArray.first { it.fieldName == key }
-                var flag = true
-                val value = ele.get(key) as String
-                if (field.type == "Number" && value.isNotEmpty() && checkPrePendingZeros(value)) {
-                    flag = false
-                }
-                if (!flag) {
-                    if (mapOfPrePendingErrors[field.fieldName] == null) {
-                        mapOfPrePendingErrors[field.fieldName] = mutableListOf()
-                    }
-                    mapOfPrePendingErrors[field.fieldName]?.add((index + 2).toString())
-                    return mapOfPrePendingErrors
-                }
-            }
+            if (validatePrependingZeroFields(element as JSONObject, fieldArray, index)) return mapOfPrePendingErrors
         }
         return mapOfPrePendingErrors
+    }
+
+    private fun validatePrependingZeroFields(
+        element: JSONObject,
+        fieldArray: Array<ConfigurationTemplate>,
+        index: Int,
+    ): Boolean {
+        val keys = element.keySet()
+        for (key in keys) {
+            val field = fieldArray.first { it.fieldName == key }
+            var flag = true
+            val value = element.get(key) as String
+            if (field.type == "Number" && value.isNotEmpty() && checkPrePendingZeros(value)) {
+                flag = false
+            }
+            if (getErrorMessages(flag, field, index)) return true
+        }
+        return false
+    }
+
+    private fun getErrorMessages(flag: Boolean, field: ConfigurationTemplate, index: Int): Boolean {
+        if (!flag) {
+            when (mapOfPrePendingErrors[field.fieldName]) {
+                null -> mapOfPrePendingErrors[field.fieldName] = mutableListOf()
+            }
+            mapOfPrePendingErrors[field.fieldName]?.add((index + 2).toString())
+            return true
+        }
+        return false
     }
 
     fun checkPrePendingZeros(value: String): Boolean {
