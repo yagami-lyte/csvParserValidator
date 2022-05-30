6f772ff055c83d1d75cc462787404a59a260c5c0
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-05-30 20:02:28 +0530

    Added cypress Test for Error section

---
+++ cypress/integration/errors.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+import 'cypress-file-upload';
+
+describe("Test for Error body",()=> {
+
+    it("Should load server successfully", () => {
+        cy.visit('http://localhost:3004')
+    })
+
+    it("Should Contain the Error Header", () => {
+        cy.contains('CSV Errors')
+    })
+
+    it("Should contain View Errors Button", () => {
+        cy.contains('View Errors')
+        cy.get('#view-errors').should('exist')
+    })
+
+    it("Should be able to click the View Errors", () => {
+        cy.get('#view-errors').click({force:true})
+    })
+
+    it("Should Contain CSV Errors as heading",()=>{
+        cy.contains('CSV Errors')
+        cy.get('h5').should('exist')
+    })
+
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
