c8e4886701acb04b3a85bfc74488113d01f6836d
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-06-01 15:42:10 +0530

    Added SpringBoot Dependencies and added route of hello World sample

---
+++ src/main/kotlin/server/Controller.kt
@@ -0,0 +1,22 @@
+package server
+
+import database.Connector
+import database.DatabaseOperations
+import org.springframework.web.bind.annotation.*
+import routeHandler.RouteHandler
+import routeHandler.getRouteHandler.getResponse.ConfigNames
+import routeHandler.getRouteHandler.getResponse.ErrorPage
+import routeHandler.getRouteHandler.getResponse.HomePage
+import routeHandler.postRouteHandler.postResponse.HandleCSVMetaData
+import routeHandler.postRouteHandler.postResponse.HandleCsv
+import routeHandler.postRouteHandler.postResponse.SendConfigurations
+
+
+@RestController
+class GreetingController {
+
+
+    @GetMapping("/{name}")
+    fun get(@PathVariable name: String) = "Hello, $name"
+
+}
