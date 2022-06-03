cf2c8e82d2a544115ff7c8e6ff11037785cfa838
yagami-lyte <usvisen2000@gmail.com>
2022-06-03 16:09:59 +0530

    added compiler options

--- src/main/kotlin/server/Main.kt
+++ src/main/kotlin/server/Main.kt
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+
 open class Application
 
 fun main(args: Array<String>) {
