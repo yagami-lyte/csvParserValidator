ea849a2ac59c9d8f6c9af141a2db6011486be55d
Utkash Singh <utkashs@Utkashs-MacBook-Pro.local>
2022-06-03 11:17:24 +0530

    Fixed cypress test

--- src/main/kotlin/server/Main.kt
+++ src/main/kotlin/server/Main.kt
@@ -13,5 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 open class Application
 
 fun main(args: Array<String>) {
+
     SpringApplication.run(Application::class.java, *args)
 }
